89173517af615270f096d811a168c389a4ef71b5
Luis Felippe <luisfelippem@gmail.com>
2025-10-09 22:49:52 -0300

    criado tipo akumaNoMi

---
+++ back-end/src/types/akumaNoMi.ts
@@ -0,0 +1,10 @@
+type akumaNoMi = {
+    _id: Number,
+    nome: string,
+    tipo: string,
+    createdAt: Date,
+    subtipo?: string,
+    tematica?: string
+    desejo?: string,
+    habilidadesBase?: string
+}
